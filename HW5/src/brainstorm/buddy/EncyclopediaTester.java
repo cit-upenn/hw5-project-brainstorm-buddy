56f6b0d6b127d66ff2c12586f5a8ce4693cba678
Gabriel Colton <gabecolton@gmail.com>
2015-12-08 15:36:28 -0500

    getting rid of meredith's stuff in order to reupdate and eventually remerge

--- HW5/src/brainstorm/buddy/EncyclopediaTester.java
+++
@@ -1,9 +0,0 @@
-package brainstorm.buddy;
-
-import java.io.IOException;
-
-public class EncyclopediaTester {
-	public static void main(String[] args) throws IOException, Exception {
-	Encyclopedia d = new Encyclopedia("test.txt", true);
-	}
-}
