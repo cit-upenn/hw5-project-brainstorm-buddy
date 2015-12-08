75421bc850c9dec9aee60949d745c1178a507115
meredm <meredithmargulies@Merediths-MacBook-Air.local>
2015-12-07 21:19:09 -0500

    Adding backend
    This is the progress of the backend thus far, still issues with
    dictionary but encyclopedia is working. Just need to output results to
    a file

---
+++ src/brainstorm/buddy/EncyclopediaTester.java
@@ -0,0 +1,9 @@
+package brainstorm.buddy;
+
+import java.io.IOException;
+
+public class EncyclopediaTester {
+	public static void main(String[] args) throws IOException, Exception {
+	Encyclopedia d = new Encyclopedia("test.txt", true);
+	}
+}
